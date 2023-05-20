76f5a29e27c0dc9269ec9c83764692870d0ae087
Vishal Tambrahalli <vishal.tambrah@gmail.com>
2023-05-20 23:40:11 +0530

    data download script

---
+++ data/data-download.sh
@@ -0,0 +1,7 @@
+# Download ModelNet10 dataset
+wget http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip
+unzip -q ModelNet10.zip
+
+# Download pre-voxelized dataset
+gdown https://drive.google.com/uc?id=1XidXoaltvr4l2oekrZM_eVFtz4DSQNFq
+unzip -q ModelNet10_voxel.zip
