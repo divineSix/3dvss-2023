f8766d8e9566e30d47d9886f2d132f28b9f313d1
Vishal Tambrahalli <vishal.tambrah@gmail.com>
2023-05-21 00:56:42 +0530

    voxnet training

--- models/voxnet.py
+++ models/voxnet.py
@@ -1,4 +1,5 @@
 import os, glob
+from collections import OrderedDict
 
 import numpy as np
 import torch
@@ -33,3 +34,38 @@ class ModelNetDataset(Dataset):
     # Overloading length class. 
     def __len__(self):
         return len(self.fpaths)
+
+# VoxNet class
+class VoxNet(torch.nn.Module):
+    def __init__(self, num_class=10, input_shape=(32,32,32)):
+        super(VoxNet, self).__init__()
+        self.num_class = num_class
+        self.input_shape = input_shape
+        self.feat = torch.nn.Sequential(OrderedDict([
+            ("conv3d_1", torch.nn.Conv3d(in_channels=1, out_channels=32, kernel_size=5, stride=2)),
+            ("relu1", torch.nn.ReLU()),
+            ("drop1", torch.nn.Dropout(p=0.2)),
+            ("conv3d_2", torch.nn.Conv3d(in_channels=32, out_channels=32, kernel_size=3)),
+            ("relu2", torch.nn.ReLU()),
+            ("pool2", torch.nn.MaxPool3d(2)),
+            ("drop2", torch.nn.Dropout(p=0.3))
+        ]))
+
+        # Calculating dimensionality for MLP input vector
+        temp = self.feat(torch.rand((1,1) + input_shape))
+        dim = 1
+        for n in temp.size()[1:]:
+            dim *= n
+        
+        self.mlp = torch.nn.Sequential(OrderedDict([
+            ("fc1", torch.nn.Linear(dim, 128)),
+            ("relu1", torch.nn.ReLU()),
+            ("drop3", torch.nn.Dropout(p=0.4)),
+            ("fc2", torch.nn.Linear(128, self.num_class))
+        ]))
+    
+    def forward(self, x):
+        x = self.feat(x)
+        x = x.view(x.size(0), -1)
+        x = self.mlp(x)
+        return x
